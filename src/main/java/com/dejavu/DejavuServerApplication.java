a0379b200a43e534fffb701be7d62ba91261d18e
komal3770 <komal3770@gmail.com>
2020-03-25 15:34:05 +0530

    first commit

---
+++ src/main/java/com/dejavu/DejavuServerApplication.java
@@ -0,0 +1,15 @@
+package com.dejavu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DejavuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DejavuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dejavu/DejavuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dejavu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DejavuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
